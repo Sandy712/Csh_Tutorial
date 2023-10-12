7832fe396e80833a9f1bb9d38d44b602e6003045
Sandeep Prajapati <sandeepprajapat200@gmail.com>
2023-10-12 12:54:53 +0530

    learn integer and operator

---
+++ 12-10-23/Program.cs
@@ -0,0 +1,77 @@
+﻿
+// int a = 18;
+// int b = 6;
+// int c = a + b;
+// Console.WriteLine(c);
+
+// int d = a / b;
+// Console.WriteLine(d);
+
+// int e = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+// Console.WriteLine(e);
+
+// int f = (a + b) / c;
+// Console.WriteLine(f);
+
+
+
+
+//Precision Integer
+
+// int a=7;
+// int b=4;
+// int c=3;
+// int d=(a+b)/c;
+// int e=(a+b)%c;
+
+// Console.WriteLine($"Quotient: {d}");
+// Console.WriteLine($"Remainder: {e}");
+
+// int max=int.MaxValue;
+// int min=int.MinValue;
+
+// Console.WriteLine($"The range of integer is {min} to {max}");
+
+// int what = max + 3;
+// Console.WriteLine($"An example of overflow: {what}");
+
+
+//Double Type
+
+// double a = 5;
+// double b = 4;
+// double c = 2;
+// double d = (a + b) / c;
+// Console.WriteLine(d);
+
+// double p=19;
+// double o=23;
+// double i=8;
+// double u=(p+o)/i;
+// Console.WriteLine(u);
+
+// double max=double.MaxValue;
+// double min=double.MinValue;
+// Console.WriteLine($"The range of double is {min} to {max}");
+
+//Decimal types
+
+// decimal min = decimal.MinValue;
+// decimal max = decimal.MaxValue;
+// Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+// double a = 1.0;
+// double b = 3.0;
+// Console.WriteLine(a / b);
+
+// decimal c = 1.0M;
+// decimal d = 3.0M;
+// Console.WriteLine(c / d);
+
+
+//Find area of cicle
+
+double radius=2.50;
+double area=Math.PI*radius*radius;
+
+Console.WriteLine(area);
