5283d66c4db3c041068248e5b27216088acc7757
Sandeep Prajapati <sandeepprajapat200@gmail.com>
2023-10-18 23:04:48 +0530

    Learn LINQ

---
+++ 18-10-23/Program.cs
@@ -0,0 +1,36 @@
+﻿// // string[] names={
+// //     "Bill",
+// //     "steve",
+// //     "James",
+// //     "Mohan"
+// // };
+
+// // var mylinq=from name in names
+// //         where name.Contains('a')
+// //         select name;
+
+// // foreach(var name in mylinq)
+// // {
+// //     Console.Write(name+" ");
+// // }
+
+
+// IList<string> stringlist =new List<string>()
+// {
+//     "C# tut",
+//     "Vb.net",
+//     "laern C.Net",
+//     "Mvc tot",
+//     "Java"
+// };
+
+// var res=from s in stringlist
+//         where s.Contains("tut")
+//         select s;
+
+// foreach(string i in res)
+// {
+//     Console.WriteLine(i);
+// }    
+
+
